cef8899f659516c2c58d6457bfc6c3f49837795f
AbhishekKaushik16 <48327447+AbhishekKaushik16@users.noreply.github.com>
2022-11-13 19:09:50 +0530

    bar chart

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactEcharts from "echarts-for-react";
 import WineData from "../assets/WineData.json";
 
 const BarChart = () =>{
+  const [data, setData] = useState({});
+  useEffect(() => {
+    let xAxis = [];
+    let yAxis = [];
+    let newData = {}
+
+    WineData.forEach(data => {
+      let alcoholType = data["Alcohol"]
+      let malicAcid = data["Malic Acid"];
+      if(newData[alcoholType]) {
+        newData[alcoholType]["Malic Acid"] += malicAcid;
+        newData[alcoholType]["count"] += 1;
+      }else{
+        newData[alcoholType] = {
+          "Malic Acid" : 0,
+          "count" : 0
+        }
+      }
+    });
+    
+    for(var data in newData) {
+      let totalMalicAcid = newData[data]["Malic Acid"];
+      let count = newData[data]["count"]
+      console.log(totalMalicAcid, count);
+      xAxis.push(data);
+      yAxis.push(totalMalicAcid / count);
+    }
+    setData({
+      xAxis: xAxis,
+      yAxis: yAxis
+    })
+    console.log(newData);
+  }, [])
 
 const bar = {
     xAxis: {
       type: 'category',
-      data: WineData.map(row=>row['Alcohol']),
+      data: data.xAxis,
       name: 'Alcohol Type',
     },
     yAxis: {
@@ -15,7 +48,7 @@ const bar = {
     },
     series: [
       {
-        data: [120, 200, 150, 80, 70, 110, 130],
+        data: data.yAxis,
         type: 'bar'
       }
     ]
